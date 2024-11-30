74465b37b3e88d4b07387e5f5d5fb1eda8543602
Paige Larson <larsonpel181@gmail.com>
2024-11-30 18:03:01 -0500

    Update ALU.v

--- pending/ALU.v
+++ pending/ALU.v
@@ -25,16 +25,31 @@ module ALU(
         // in an array for each instr: data from src1 and src2/imm, aluOp, which FU they go to 
 input clk, 
 input rstn,
-input [31:0] data_in [2:0] [3:0],
+input func7,
+input func3,
+input opcode,
+
+input [31:0] data_in_sr1 [2:0],
+input [31:0] data_in_sr2 [2:0],
+input [31:0] data_in_imm [2:0],
+input [5:0] dr_in [2:0],
+
 
 //outputs: data for dest reg
-output reg [31:0] data_out [2:0]
+output reg [31:0] data_out_dr [2:0],
+output reg [5:0] dr_out [2:0],
+output reg FU [2:0]
+
+
+
+
+
     );
     
-    reg FU [2:0];
+    
         
     integer i;
-    always @(posedge clk or negedge rstn) begin
+    always @(*) begin
             if (~rstn)begin
                 FU[0]=1'b0;
                 FU[1]=1'b0;
@@ -42,20 +57,112 @@ output reg [31:0] data_out [2:0]
             end 
             else begin
                 for (i=0; i<2; i=i+1) begin
-                    if(FU[data_in[i][3]])begin
-                        FU[data_in[i][3]]=1'b1;
-                        //TODO: 
-                        //run actual alu store in data_out[i]
+                    if(FU[i]==1'b0)begin //if FU is available
+                    
+                        FU[i]=1'b1;                       
+                        //huge switch statement to determine alu type via alu op
+                        if(opcode==7'b0110011) begin //Rtype
+                            if (func3==3'b000)begin
+                                if (func7==7'b0000000)begin
+                                    //ADD
+                                    data_out_dr[i]=data_in_sr1[i]+data_in_sr2[i];
+                                end
+                                if (func7==7'b0100000)begin
+                                    //SUB
+                                    data_out_dr[i]=data_in_sr1[i]-data_in_sr2[i];
+                                end
+                            end
+                            else if (func3==3'b001)begin
+                                //SLL
+                                display("unexpected instruction");
+                            end
+                            else if (func3==3'b010)begin
+                                //SLT
+                                display("unexpected instruction");
+                            end
+                            else if (func3==3'b011)begin
+                                //SLTU
+                                display("unexpected instruction");
+                            end
+                            else if (func3==3'b100)begin
+                                //XOR
+                                data_out_dr[i]=data_in_sr1[i] ^ data_in_sr2[i];
+                            end
+                            else if (func3==3'b101)begin
+                                //SR
+                                if (func7==7'b0000000)begin
+                                    //SRL
+                                    display("unexpected instruction");
+                                end
+                                if (func7==7'b0100000)begin
+                                    //SRA
+                                    display("unexpected instruction");
+                                end
+                            end
+                            else if (func3==3'b110)begin
+                                //OR
+                                data_out_dr[i]=data_in_sr1[i] | data_in_sr2[i];
+                            end
+                            else if (func3==3'b111)begin
+                                //AND
+                                data_out_dr[i]=data_in_sr1[i] & data_in_sr2[i];
+                            end
+                        end
+                        else if(opcode==7'b0010011) begin //Itype
+                            if (func3==3'b000)begin
+                                //ADDI
+                                data_out_dr[i]=data_in_sr1[i]+data_in_imm[i];
+                            end
+                            else if (func3==3'b001)begin
+                                //SLLI
+                                display("unexpected instruction");
+                            end
+                            else if (func3==3'b010)begin
+                                //SLTI
+                                display("unexpected instruction");
+                            end
+                            else if (func3==3'b011)begin
+                                //SLTIU
+                                display("unexpected instruction");
+                            end
+                            else if (func3==3'b100)begin
+                                //XORI
+                                data_out_dr[i]=data_in_sr1[i] ^ data_in_imm[i];
+                            end
+                            else if (func3==3'b101)begin
+                                //SR
+                                if (func7==7'b0000000)begin
+                                    //SRLI
+                                    display("unexpected instruction");
+                                end
+                                if (func7==7'b0100000)begin
+                                    //SRAI
+                                    data_out_dr[i]=data_in_sr1[i]>>data_in_imm[i];
+                                end
+                            end 
+                            else if (func3==3'b110)begin
+                                //ORI
+                                data_out_dr[i]=data_in_sr1[i] | data_in_imm[i];
+                            end 
+                            else if (func3==3'b1111)begin
+                                //ANDI
+                                data_out_dr[i]=data_in_sr1[i] & data_in_imm[i];
+                            end
+                        end
+                        else if(opcode==7'b0110111) begin // Utype
+                            //LUI
+                            data_out_dr[i]=data_in_imm[i]<<12;
+                        end
+                        else if(opcode==7'b0000011 || opcode==7'b0110011) begin //load/store
+                        //do nothing
+                        end
                         
-                            //huge switch statement to determine alu type via alu op
-                            if(data_in[i][2]=)
+                        FU[i]=1'b0;
                         
-                        data_out[i]=31'b0;
                     end
                 end
             end
     end
 endmodule
-
 
 
