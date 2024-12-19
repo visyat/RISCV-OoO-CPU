5416b55a7026e0da53edff2c5396eb5f4141bf68
Vishal Yathish <vishal.yathish@gmail.com>
2024-12-18 23:20:34 -0800

    refactored LSQ

--- LSQ.v
+++ LSQ.v
@@ -51,9 +51,10 @@ module Load_Store_Queue (
     reg [31:0] LSQ_DATA [15:0];
     reg [15:0] ISSUED;
 
-    integer i,j;
-    
-    always @(posedge clk or negedge rstn) begin
+    integer i, j, k, m;
+    reg [3:0] index = 0;
+
+    always @(posedge clk or negedge rstn) begin // initialize LSQ entries, reserve entries on dispatch ...
         if (~rstn) begin // on reset, set all LSQ entries to 0 ...
             VALID = 16'b0;
             OP = 16'b0;
@@ -64,12 +65,7 @@ module Load_Store_Queue (
                 ADDRESS[i] = 32'b0;
                 LSQ_DATA[i] = 32'b0;
             end
-            pcOut = 32'b0;
-            addressOut = 32'b0;
-            fromLSQ = 0;
-            lwData = 32'b0;
-            complete = 0;
-        end else begin 
+        end else begin
             // dispatch logic ... if read/write, reserve space in LSQ
             if (memRead || memWrite) begin
                 for (i=0; i<16; i=i+1) begin
@@ -85,66 +81,85 @@ module Load_Store_Queue (
                     end
                 end
             end
-
-            // execution logic ... if update address in LSQ entry; if load, scan LSQ to find matching addresses, provide data for latest store
-            for (i=0; i<16; i=i+1) begin
-                if (PC[i] == pcLsu) begin
-                    ADDRESS[i] = addressLsu;
-                    j = i;
-                    i=16;
-                end
-            end
-            for (i=15; i>=0; i=i-1) begin
-                if (ADDRESS[i] == ADDRESS[j]) begin
-                    LSQ_DATA[j] = LSQ_DATA[i]; // populate LW data with the most recent store to the same address
-                    i=-1;
-                end
-            end
-
-            // issue logic ... complete loads if data exists, else issue most recent available instruction 
-            for (i=0; i<16; i=i+1) begin
-                if (VALID[i] && ~ISSUED[i] && ~OP[i] && LSQ_DATA[i] != 32'b0) begin
-                    pcOut = PC[i];
-                    addressOut = ADDRESS[i];
-                    lwData = LSQ_DATA[i];
-                    fromLSQ = 1;
-                    complete = 1;
-                    loadStore = 0;
-                    storeSizeOut = SIZE[i];
-                    swDataOut = 32'b0;
-                    ISSUED[i] = 1;
-                    i=16;
-                end
-            end
-            for (i=0; i<16; i=i+1) begin
-                if (~complete && VALID[i] && ~ISSUED[i]) begin
-                    pcOut = PC[i];
-                    addressOut = ADDRESS[i];
-                    lwData = 32'b0;
-                    fromLSQ = 0;
-                    complete = 0;
-                    loadStore = OP[i];
-                    storeSizeOut = SIZE[i];
-                    swDataOut = LSQ_DATA[i];
-                    ISSUED[i] = 1;
-                    i=16;
-                end
-            end
-
-            // retirement logic ... deallocate LSQ entry
-            for (i=0; i<16; i=i+1) begin
-                if (pcRet1 == PC[i] || pcRet2 == PC[i]) begin
-                    VALID[i] = 0;
-                    PC[i] = 0;
-                    OP[i] = 0;
-                    SIZE[i] = 0;
-                    ADDRESS[i] = 32'b0;
-                    LSQ_DATA[i] = 32'b0;
-                    ISSUED[i] = 0;
-                    i=16;
-                end
-            end
-
         end
     end
+
+    always @(*) begin // handle broadcasted addresses ...
+        for (j=0; j<16; j=j+1) begin
+            if (PC[j] == pcLsu) begin
+                ADDRESS[j] = addressLsu;
+                index = j;
+                j = 17;
+            end
+        end
+        // execution logic ... if update address in LSQ entry; if load, scan LSQ to find matching addresses, provide data for latest store
+        if (~OP[index]) begin
+            for (j=15; j>=0; j=j-1) begin
+                if (ADDRESS[index] == ADDRESS[j] && j != index) begin
+                    LSQ_DATA[index] = LSQ_DATA[j]; // populate LW data with the most recent store to the same address
+                    j = -1;
+                end
+            end
+        end 
+    end
+    always @(*) begin // handle broadcasted retirement instructions ...
+        // retirement logic ... deallocate LSQ entry
+        for (k=0; k<16; k=k+1) begin
+            if (pcRet1 == PC[k] || pcRet2 == PC[k]) begin
+                VALID[k] = 0;
+                PC[k] = 0;
+                OP[k] = 0;
+                SIZE[k] = 0;
+                ADDRESS[k] = 32'b0;
+                LSQ_DATA[k] = 32'b0;
+                ISSUED[k] = 0;
+                k=16;
+            end
+        end
+    end
+
+    always @(posedge clk or negedge rstn) begin // handle issue logic ...
+        if (~rstn) begin
+            pcOut = 'b0;
+            addressOut = 'b0;
+            lwData = 'b0;
+            fromLSQ = 'b0;
+            loadStore = 'b0;
+            storeSizeOut = 'b0;
+            swDataOut = 'b0;
+            complete = 'b0;
+        end else begin
+            fromLSQ = 'b0;
+            // issue logic ... complete loads if data exists, else issue most recent available instruction 
+            for (m=0; m<16; m=m+1) begin
+                if (VALID[m] && ~ISSUED[m] && ~OP[m] && LSQ_DATA[m] != 32'b0) begin
+                    pcOut = PC[m];
+                    addressOut = ADDRESS[m];
+                    lwData = LSQ_DATA[m];
+                    fromLSQ = 1;
+                    loadStore = 0;
+                    storeSizeOut = SIZE[m];
+                    swDataOut = 32'b0;
+                    ISSUED[m] = 1;
+                    i=16;
+                end
+            end
+            if (~fromLSQ) begin
+                for (m=0; m<16; m=m+1) begin
+                    if (VALID[m] && ~ISSUED[m]) begin
+                        pcOut = PC[m];
+                        addressOut = ADDRESS[m];
+                        lwData = 'b0;
+                        fromLSQ = 0;
+                        loadStore = OP[m];
+                        storeSizeOut = SIZE[m];
+                        swDataOut = LSQ_DATA[m];
+                        ISSUED[m] = 1;
+                        i=16;
+                    end
+                end
+            end
+        end
+    end
+    
 endmodule
