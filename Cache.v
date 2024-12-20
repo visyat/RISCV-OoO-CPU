ea8da85277a67bdf767d9cc8e70ca850b02d2874
Vishal Yathish <vishal.yathish@gmail.com>
2024-12-19 16:35:38 -0800

    reimplemented caching: parallelized searches of ways and sequential insertion; prevents duplicates

--- Cache.v
+++ Cache.v
@@ -15,13 +15,6 @@ module Cache(
 
     output reg [31:0] lw_data,
     output reg cacheMiss
-
-    // output reg [31:0] PC_out, 
-    // output reg [31:0] address_out, 
-    // output memRead_out,
-    // output memWrite_out, 
-    // output storeSize_out, 
-    // output fromLSQ_out, 
 );
     reg [127:0] VALID_WAY_1;
     reg [18:0] TAG_WAY_1 [0:127];
@@ -39,7 +32,12 @@ module Cache(
     reg [18:0] TAG_WAY_4 [0:127];
     reg [511:0] DATA_WAY_4 [0:127];
 
-    integer i;
+    integer i, j, k, m, n;
+
+    reg found_1;
+    reg found_2;
+    reg found_3;
+    reg found_3;
 
     reg [31:0] search_1;
     reg [31:0] search_2;
@@ -54,157 +52,182 @@ module Cache(
                 TAG_WAY_1[i] = 'b0;
                 DATA_WAY_1[i] = 'b0;
             end 
-            search_1 = 'b1;
+            search_1 = 'b0;
+            found_1 = 'b0;
         end else begin
-            if (TAG_WAY_1[address_in[12:6]] == address_in[31:13] && VALID_WAY_1[address_in[12:6]]) begin
-                if (memRead) begin
+            if (memRead) begin
+                if (TAG_WAY_1[address_in[12:6]] == address_in[31:13] && VALID_WAY_1[address_in[12:6]]) begin
+                    found_1 = 'b1;
                     search_1 = {24'b0, DATA_WAY_1[address_in[12:6]][7:0]};
+                end else begin
+                    found_1 = 'b0;
+                    search_1 = 'b0;
                 end
-                if (memWrite) begin
-                    if (storeSize) begin
-                        DATA_WAY_1[address_in[12:6]][7:0] = data_sw;
-                    end else begin
-                        DATA_WAY_1[address_in[12:6]][15:8] = data_sw[15:8];
-                        DATA_WAY_1[address_in[12:6]][7:0] = data_sw[7:0];
-                    end
-                    search_1 = 'b1;
-                end
-            end else begin
-                search_1 = 'b1;
-            end
+            end 
         end
     end
     always @(posedge clk or negedge rstn) begin
         // search way 2 ...
         if (~rstn) begin
             VALID_WAY_2 = 'b0;
-            for (i=0; i<128; i=i+1) begin
-                TAG_WAY_2[i] = 'b0;
-                DATA_WAY_2[i] = 'b0;
+            for (j=0; j<128; j=j+1) begin
+                TAG_WAY_2[j] = 'b0;
+                DATA_WAY_2[j] = 'b0;
             end 
-            search_2 = 'b1;
+            search_2 = 'b0;
+            found_2 = 'b0;
         end else begin
-            if (TAG_WAY_2[address_in[12:6]] == address_in[31:13] && VALID_WAY_2[address_in[12:6]]) begin
-                if (memRead) begin
+            if (memRead) begin
+                if (TAG_WAY_2[address_in[12:6]] == address_in[31:13] && VALID_WAY_2[address_in[12:6]]) begin
+                    found_2 = 'b1;
                     search_2 = {24'b0, DATA_WAY_2[address_in[12:6]][7:0]};
+                end else begin
+                    found_2 = 'b0;
+                    search_2 = 'b0;
                 end
-                if (memWrite) begin
-                    if (storeSize) begin
-                        DATA_WAY_2[address_in[12:6]][7:0] = data_sw;
-                    end else begin
-                        DATA_WAY_2[address_in[12:6]][15:8] = data_sw[15:8];
-                        DATA_WAY_2[address_in[12:6]][7:0] = data_sw[7:0];
-                    end
-                    search_2 = 'b1;
-                end
-            end else begin
-                search_2 = 'b1;
-            end
+            end 
         end
     end
     always @(posedge clk or negedge rstn) begin
         // search way 3 ...
         if (~rstn) begin
             VALID_WAY_3 = 'b0;
-            for (i=0; i<128; i=i+1) begin
-                TAG_WAY_3[i] = 'b0;
-                DATA_WAY_3[i] = 'b0;
+            for (k=0; k<128; k=k+1) begin
+                TAG_WAY_3[k] = 'b0;
+                DATA_WAY_3[k] = 'b0;
             end 
-            search_3 = 'b1;
+            search_3 = 'b0;
+            found_3 = 'b0;
         end else begin
-            if (TAG_WAY_3[address_in[12:6]] == address_in[31:13] && VALID_WAY_3[address_in[12:6]]) begin
-                if (memRead) begin
+            if (memRead) begin
+                if (TAG_WAY_3[address_in[12:6]] == address_in[31:13] && VALID_WAY_3[address_in[12:6]]) begin
+                    found_3 = 'b1;
                     search_3 = {24'b0, DATA_WAY_3[address_in[12:6]][7:0]};
+                end else begin
+                    found_3 = 'b0;
+                    search_3 = 'b0;
                 end
-                if (memWrite) begin
-                    if (storeSize) begin
-                        DATA_WAY_3[address_in[12:6]][7:0] = data_sw;
-                    end else begin
-                        DATA_WAY_3[address_in[12:6]][15:8] = data_sw[15:8];
-                        DATA_WAY_3[address_in[12:6]][7:0] = data_sw[7:0];
-                    end
-                    search_3 = 'b1;
-                end
-            end else begin
-                search_3 = 'b0;
-            end
+            end 
         end
     end
     always @(posedge clk or negedge rstn) begin
         // search way 4 ...
         if (~rstn) begin
             VALID_WAY_4 = 'b0;
-            for (i=0; i<128; i=i+1) begin
-                TAG_WAY_4[i] = 'b0;
-                DATA_WAY_4[i] = 'b0;
+            for (m=0; m<128; m=m+1) begin
+                TAG_WAY_4[m] = 'b0;
+                DATA_WAY_4[m] = 'b0;
             end 
-            search_4 = 'b1;
+            search_4 = 'b0;
+            found_4 = 'b0;
         end else begin
-            if (TAG_WAY_4[address_in[12:6]] == address_in[31:13] && VALID_WAY_4[address_in[12:6]]) begin
-                if (memRead) begin
+            if (memRead) begin
+                if (TAG_WAY_4[address_in[12:6]] == address_in[31:13] && VALID_WAY_4[address_in[12:6]]) begin
+                    found_4 = 'b1;
                     search_4 = {24'b0, DATA_WAY_4[address_in[12:6]][7:0]};
+                end else begin
+                    found_4 = 'b0;
+                    search_4 = 'b0;
                 end
-                if (memWrite) begin
+            end 
+        end
+    end
+
+    always @(posedge clk or negedge rstn) begin
+        // output ... 
+        if (~rstn) begin
+            lw_data = 'b0;
+            cacheMiss = 'b0;
+        end else begin
+            if (memRead) begin
+                if (found_1) begin
+                    lw_data = search_1;
+                    cacheMiss = 'b0;
+                end else if (found_2) begin
+                    lw_data = search_2;
+                    cacheMiss = 'b0;
+                end else if (found_3) begin
+                    lw_data = search_3;
+                    cacheMiss = 'b0;
+                end else if (found_4) begin
+                    lw_data = search_4;
+                    cacheMiss = 'b0;
+                end else begin
+                    lw_data = 'b0;
+                    cacheMiss = 'b1;
+                end
+            end else if (memWrite) begin
+                lw_data = 'b0;
+                // find first way at the designated index that is available ...
+                if (~VALID_WAY_1[address_in[12:6]]) begin
+                    VALID_WAY_1[address_in[12:6]] = 1;
+                    TAG_WAY_1[address_in[12:6]] = address_in[31:13];
                     if (storeSize) begin
                         DATA_WAY_4[address_in[12:6]][7:0] = data_sw;
                     end else begin
                         DATA_WAY_4[address_in[12:6]][15:8] = data_sw[15:8];
                         DATA_WAY_4[address_in[12:6]][7:0] = data_sw[7:0];
                     end
-                    search_4 = 'b1;
-                end
-            end else begin
-                search_4 = 'b1;
-            end
-        end
-    end
 
-    always @(posedge clk) begin
-        // output ... 
-        if (~rstn) begin
-            // PC_out = 'b0;
-            lw_data = 'b0;
-            cacheMiss = 0;
-            // address_out = 'b0;
-            // memWrite_out = 0;
-            // memRead_out = 0;
-            // storeSize_out = 0;
-            // fromLSQ_out = 0;
-        end else begin
-            // PC_out = PC_in;
-            // address_out = address_in;
-            // memWrite_out = memWrite;
-            // memRead_out = memRead;
-            // storeSize_out = storeSize;
-            // fromLSQ_out = fromLSQ;
-            if (memRead) begin
-                if (search_1 != 'b1) begin
-                    lw_data = search_1;
-                    cacheMiss = 0;
-                end else if (search_2 != 'b1) begin
-                    lw_data = search_2;
-                    cacheMiss = 0;
-                end else if (search_3 != 'b1) begin
-                    lw_data = search_3;
-                    cacheMiss = 0;
-                end else if (search_4 != 'b1) begin
-                    lw_data = search_4;
-                    cacheMiss = 0;
-                end else begin
-                    lw_data = 'b0;
-                    cacheMiss = 1;
+                end else if (~VALID_WAY_2[address_in[12:6]]) begin
+                    VALID_WAY_2[address_in[12:6]] = 1;
+                    TAG_WAY_2[address_in[12:6]] = address_in[31:13];
+                    if (storeSize) begin
+                        DATA_WAY_4[address_in[12:6]][7:0] = data_sw;
+                    end else begin
+                        DATA_WAY_4[address_in[12:6]][15:8] = data_sw[15:8];
+                        DATA_WAY_4[address_in[12:6]][7:0] = data_sw[7:0];
+                    end
+
+                end else if (~VALID_WAY_3[address_in[12:6]]) begin
+                    VALID_WAY_3[address_in[12:6]] = 1;
+                    TAG_WAY_2[address_in[12:6]] = address_in[31:13];
+                    if (storeSize) begin
+                        DATA_WAY_4[address_in[12:6]][7:0] = data_sw;
+                    end else begin
+                        DATA_WAY_4[address_in[12:6]][15:8] = data_sw[15:8];
+                        DATA_WAY_4[address_in[12:6]][7:0] = data_sw[7:0];
+                    end
+
+                end else if (~VALID_WAY_4[address_in[12:6]]) begin
+                    VALID_WAY_4[address_in[12:6]] = 1;
+                    TAG_WAY_2[address_in[12:6]] = address_in[31:13];
+                    if (storeSize) begin
+                        DATA_WAY_4[address_in[12:6]][7:0] = data_sw;
+                    end else begin
+                        DATA_WAY_4[address_in[12:6]][15:8] = data_sw[15:8];
+                        DATA_WAY_4[address_in[12:6]][7:0] = data_sw[7:0];
+                    end
+
+                end else begin // need to evict a cache entry ... implement random eviction!
+                    TAG_WAY_1[address_in[12:6]] = address_in[31:13];
+                    if (storeSize) begin
+                        DATA_WAY_4[address_in[12:6]][7:0] = data_sw;
+                    end else begin
+                        DATA_WAY_4[address_in[12:6]][15:8] = data_sw[15:8];
+                        DATA_WAY_4[address_in[12:6]][7:0] = data_sw[7:0];
+                    end
                 end
-            end
-            
-            if (memWrite) begin
-                lw_data = 'b1;
-                cacheMiss = 1;
+                cacheMiss = 'b1;
             end
             if (fromLSQ) begin
                 lw_data = 'b0;
-                cacheMiss = 0;
+                cacheMiss = 'b0;
             end
         end
     end
 
+/*
+    if (memWrite) begin
+        if (storeSize) begin
+            DATA_WAY_4[address_in[12:6]][7:0] = data_sw;
+        end else begin
+            DATA_WAY_4[address_in[12:6]][15:8] = data_sw[15:8];
+            DATA_WAY_4[address_in[12:6]][7:0] = data_sw[7:0];
+        end
+        search_4 = 'b1;
+    end
+*/
+
 endmodule
+
